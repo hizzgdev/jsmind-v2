12c9a748ff8cf92234e9e89f62f37b9a99d18301
hizzgdev <hizzgdev@163.com>
2025-12-27 17:02:06 +0800

    setup jsdom for testing

--- __tests__/jsmind.test.ts
+++ __tests__/jsmind.test.ts
@@ -2,6 +2,8 @@ import assert from 'node:assert/strict';
 import test from 'node:test';
 import JsMind from '../src/jsmind.ts';
 import { type JsMindOptions } from '../src/jsmind.const.ts';
+import './setup/jsdom.test.ts';
+import { JSMIND_CONTAINER_ID } from './setup/jsdom.test.ts';
 
 test('JsMind static', () => {
     assert.ok(JsMind.Version);
@@ -10,7 +12,7 @@ test('JsMind static', () => {
 );
 
 test('jsmind', () => {
-    const opts: JsMindOptions = { container: 'test', mind: { rootNodeId: 'test' }, viewOptions: { theme: 'test' } };
+    const opts: JsMindOptions = { container: JSMIND_CONTAINER_ID, mind: { rootNodeId: 'test' }, viewOptions: { theme: 'test' } };
     const jm = new JsMind(opts);
     assert.ok(jm);
     assert.strictEqual(JsMind.Version, '2.0');
